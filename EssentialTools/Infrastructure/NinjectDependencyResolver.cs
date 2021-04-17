8f8e6f9d9c0c1299fd4fe4a6ae5e27c5dba74ef3
Yanis <allseeingeye1315@gmail.com>
2021-04-17 21:04:47 +0300

    Изменяем метод AddBindigs() класса NinjectDependencyResolver, чтобы сообщить Ninject, когда необходимо использовать каждый из классов

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -33,7 +33,9 @@ namespace EssentialTools.Infrastructure
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
             kernel.Bind<IDiscountHelper>()
-               .To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
+              .To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
+            kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>()
+              .WhenInjectedInto<LinqValueCalculator>();
         }
     }
 }
