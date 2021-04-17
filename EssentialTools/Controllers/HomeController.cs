41838aa6b0b56ce12e736251bef1e48994598ebb
Yanis <allseeingeye1315@gmail.com>
2021-04-17 12:39:21 +0300

    Добавляем в папку Controllers новый контроллер по имени HomeController с содержимым

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using EssentialTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product {Name = "Каяк", Category = "Водные виды спорта", Price = 275M},
+            new Product {Name = "Спасательный жилет", Category = "Водные виды спорта", Price = 48.95M},
+            new Product {Name = "Мяч", Category = "Футбол", Price = 19.50M},
+            new Product {Name = "Угловой флажок", Category = "Футбол", Price = 34.95M}
+        };
+
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc)
+            {
+                Products = products
+            };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
