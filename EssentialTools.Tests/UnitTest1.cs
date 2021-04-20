4861d6c7cc83f8423b46ecfba9a2cfa0b55bcbb4
Yanis <allseeingeye1315@gmail.com>
2021-04-20 16:22:11 +0300

    К тестовому проекту  добавляем дальнейшие тесты, предназначенные для проверки других аспектов поведения класса MinimumDiscountHelper

--- EssentialTools.Tests/UnitTest1.cs
+++ EssentialTools.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using EssentialTools.Models;
 using NUnit.Framework;
 
@@ -23,6 +24,50 @@ namespace EssentialTools.Tests
 
             // assert (утверждение)
             Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            // arrange (организация)
+            IDiscountHelper target = getTestObject();
+
+            // act (акт)
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            // assert (утверждение)
+            Assert.AreEqual(5, TenDollarDiscount, "$10 разница");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 разница");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 разница");
+        }
+
+        [TestMethod]
+        public void Discount_Less_Than_10()
+        {
+            // arrange (организация)
+            IDiscountHelper target = getTestObject();
+
+            // act (акт)
+            decimal discount5 = target.ApplyDiscount(5);
+            decimal discount0 = target.ApplyDiscount(0);
+
+            // assert (утверждение)
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Discount_Negative_Total()
+        {
+            // arrange (организация) 
+            IDiscountHelper target = getTestObject();
+
+            // act (акт)
+            target.ApplyDiscount(-1);
         }
     }
 }
